7235bf32ba51c034137ebfddccfcad803853ab03
MehdiAIO <elmahdi.aitouzdi@gmail.com>
2024-03-30 13:50:30 +0000

    2nd commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require("fs");
 const equipes = require("./equipes.json");
 const express = require("express");
 
@@ -51,12 +51,12 @@ app.get("/equipes/:id", (req, res) => {
 app.put("/equipes/:id", (req, res) => {
   try {
     const id = parseInt(req.params.id);
-    const equipe = equipes.find(equipe => equipe.id === id);
+    const equipe = equipes.find((equipe) => equipe.id === id);
     if (!equipe) {
       return res.status(404).json({ message: "Not found" });
     }
-    equipe.name= req.body.name || equipe.name;
-    equipe.age= req.body.age  || equipe.age;
+    equipe.name = req.body.name || equipe.name;
+    equipe.age = req.body.age || equipe.age;
     equipe.gender = req.body.gender || equipe.gender;
     res.status(200).json(equipe);
     saveData();
@@ -66,17 +66,58 @@ app.put("/equipes/:id", (req, res) => {
 });
 
 // delete
-app.delete("/equipes/:id",(req,res) => {
-  try{
+app.delete("/equipes/:id", (req, res) => {
+  try {
     const id = parseInt(req.params.id);
-    const equipeIndex = equipes.findIndex(equipe => equipe.id === id);
-    if(equipeIndex === -1){
-      return res.status(400).json({message:"equipe introuvable"})
+    const equipeIndex = equipes.findIndex((equipe) => equipe.id === id);
+    if (equipeIndex === -1) {
+      return res.status(400).json({ message: "equipe introuvable" });
     }
-    equipes.splice(equipeIndex,1);
+    equipes.splice(equipeIndex, 1);
     res.status(200).json(equipes);
     saveData();
-  }catch(err){
-    res.status(500).json({message : err});
+  } catch (err) {
+    res.status(500).json({ message: err });
   }
 });
+
+// Route qui affiche tous les jouers d'une équipe
+app.get("/equipes/joueurs/:id", (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    const equipe = equipes.filter((equipe) => equipe.idEquipe === id);
+    res.status(200).json(equipe);
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+});
+
+// Route qui affiche l'equipe d'un joueur en fonction de son id
+app.get("/equipes/equipe/:id", (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    const equipe = equipes.find((equipe) => equipe.id === id);
+    if (!equipe) {
+      return res.status(400).json({ message: "Equipe Introuvable" });
+    }
+    const equipeJoueur = equipe.equipeNom;
+    res.status(200).json(equipeJoueur);
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+});
+
+// Route qui affiche un joueur en fonction de son nom
+app.get("/equipes/joueur/:nom", (req, res) => {
+  try {
+    const nom = (req.params.nom).toLowerCase();
+    const joueur = equipes.find((equipe) => equipe.nom === nom);
+    if (!joueur) {
+      return res.status(400).json({ message: "Le joueur est introuvable" });
+    }
+    res.status(200).json(joueur);
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+});
+
